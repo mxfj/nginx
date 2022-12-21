47c608a86d0c719bc6173b020f5b876ac19f5b0c
Hongbin <13405999037@139.com>
2022-12-21 11:20:41 +0800

    Normol commit

--- git-push.sh
+++ git-push.sh
@@ -1,3 +1,6 @@
+git config --global user.name "Hongbin"
+git config --global user.email 13405999037@139.com
+
 git add .
 git commit -m "Normol commit"
 # git remote add origin git@github.com:mxfj/nginx.git
