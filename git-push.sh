56b4ce1ea52f6267770ab97062527c6b050735d1
Hongbin <13405999037@139.com>
2022-12-22 09:05:25 +0800

    Normol commit

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,5 @@
-git config --global user.name "Hongbin"
-git config --global user.email 13405999037@139.com
+# git config --global user.name "Hongbin"
+# git config --global user.email 13405999037@139.com
 
 git add .
 git commit -m "Normol commit"
